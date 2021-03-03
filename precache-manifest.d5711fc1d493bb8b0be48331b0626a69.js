10ae8259306913dc42f17101c950428ce7ae91cb
acer <sld377575@gmail.com>
2021-03-03 06:42:28 +0600

    Updates

---
+++ precache-manifest.d5711fc1d493bb8b0be48331b0626a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff137aac94a6ca10e12e523861154bb",
+    "url": "/react-deep-speech-test/index.html"
+  },
+  {
+    "revision": "f3be63c88c6328594117",
+    "url": "/react-deep-speech-test/static/css/main.9a13c7fb.chunk.css"
+  },
+  {
+    "revision": "659e14a656837c8e619d",
+    "url": "/react-deep-speech-test/static/js/2.e9728b9c.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/react-deep-speech-test/static/js/2.e9728b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3be63c88c6328594117",
+    "url": "/react-deep-speech-test/static/js/main.b71f67c2.chunk.js"
+  },
+  {
+    "revision": "2c47ac75be1fe11076a2",
+    "url": "/react-deep-speech-test/static/js/runtime-main.37fa1e16.js"
+  }
+]);
